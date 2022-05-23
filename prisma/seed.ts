f56045821ead18b5b0356429e9d71a634985029e
Ruan Belem <ruan.belem@tpv-tech.com>
2022-05-23 10:04:14 -0400

    Adicionando código para semear o banco com dados dos clubes.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -28,7 +28,77 @@ async function main() {
             nome: "Diretor do Oanse",
             descricao: "Orienta os diretores de clube."
         }
-    })
+    });
+
+    const Secretario = await prisma.cargo.upsert({
+        where: {id_cargo: 4},
+        update: {},
+        create: {
+            nome: "Secretário",
+            descricao: "Gerencia os registros e materiais do clube."
+        }
+    });
+
+    const clubeUrsinhos = await prisma.clube.upsert({
+        where: {id_clube: 1},
+        update: {},
+        create: {
+            nome: "Ursinhos",
+            idade_entrada: 4,
+            idade_saida: 5
+        }
+    });
+
+    const clubeFaiscas = await prisma.clube.upsert({
+        where: {id_clube: 2},
+        update: {},
+        create: {
+            nome: "Faíscas",
+            idade_entrada: 6,
+            idade_saida: 8
+        }
+    });
+
+    const clubeFlama = await prisma.clube.upsert({
+        where: {id_clube: 3},
+        update: {},
+        create: {
+            nome: "Flama",
+            idade_entrada: 9,
+            idade_saida: 10
+        }
+    });
+
+    const clubeTocha = await prisma.clube.upsert({
+        where: {id_clube: 4},
+        update: {},
+        create: {
+            nome: "Tocha",
+            idade_entrada: 11,
+            idade_saida: 12
+        }
+    });
+
+    const clubeJV = await prisma.clube.upsert({
+        where: {id_clube: 5},
+        update: {},
+        create: {
+            nome: "JV",
+            idade_entrada: 13,
+            idade_saida: 14
+        }
+    });
+
+    const clubeVQ7= await prisma.clube.upsert({
+        where: {id_clube: 6},
+        update: {},
+        create: {
+            nome: "VQ7",
+            idade_entrada: 15,
+            idade_saida: 17
+        }
+    });
+
 }
 
 main()
